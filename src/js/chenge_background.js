29f70c96fe77149163c4ff4fc33b20603c0aa2d6
AlexSytsenko <sytsenko@gmail.com>
2021-01-19 12:09:23 +0200

    создает переключатель фона

--- src/js/chenge_background.js
+++ src/js/chenge_background.js
@@ -6,8 +6,28 @@ const colors = [
   '#009688',
   '#795548',
 ];
+const body = document.body;
+const btnStart = document.querySelector('[data-action="start"]');
+const btnStop = document.querySelector('[data-action="stop"]');
+let timerId = null;
 
+function chengeBackground() {
+  body.style = `background-color: ${colors[randomIntegerFromInterval(0, colors.length - 1)]}`;
+}
+
+function stopHandler() {
+  clearInterval(timerId);
+  btnStart.disabled = false;
+}
 
 const randomIntegerFromInterval = (min, max) => {
   return Math.floor(Math.random() * (max - min + 1) + min);
 };
+
+// Слушатели
+btnStart.addEventListener('click', () => {
+  timerId = setInterval(chengeBackground, 1000);
+  btnStart.disabled = true; 
+});
+btnStop.addEventListener('click', stopHandler);
+
